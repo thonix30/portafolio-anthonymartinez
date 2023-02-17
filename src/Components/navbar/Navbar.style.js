958f369b7258c672255ad8e22fe2b75232ddfd75
thonix30 <anthonye30am@gmail.com>
2023-02-17 16:33:16 -0600

    feat:add global colors

--- src/Components/navbar/Navbar.style.js
+++ src/Components/navbar/Navbar.style.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
+import { colors } from "../../styles/colors";
 
 export const  ContainerNavbar = styled.div`
 height: 100px;
-background-color: red;
+background-color: ${colors.TEXT_BLACK};
 `;
