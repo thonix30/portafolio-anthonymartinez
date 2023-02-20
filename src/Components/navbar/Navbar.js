2d153c56bec0de7c84c2eac638b5c2875593c0c8
thonix30 <anthonye30am@gmail.com>
2023-02-20 17:36:30 -0600

    feat: add bruguer menu movile and desktop

--- src/Components/navbar/Navbar.js
+++ src/Components/navbar/Navbar.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './Navbar.style';
 
 const Navbar = () => {
-  return <S.ContainerNavbar>Nuestro container navbar</S.ContainerNavbar>;
-  
+
+  const [showMenuBurguer, setShowMenuBurguer] = useState(false);
+
+  console.log(showMenuBurguer);
+  return (
+    <S.ContainerNavbar>  
+      <S.ContainerLogo>
+        <S.ImageLogo src='/img/LOGO ANTHONY.png' alt='logo anthony martinez' />
+        <div>
+          <h1>Anthony Martínez</h1>
+          <h2>Frontend Developer</h2>
+        </div>
+      </S.ContainerLogo>
+      <S.ContainerBurguer onClick={() => setShowMenuBurguer(!showMenuBurguer)}>
+        <S.BurguerMenu />
+        <S.BurguerMenu />
+        <S.BurguerMenu />
+      </S.ContainerBurguer>
+      <S.ContainerItems showMenuBurguer={showMenuBurguer}>
+        <p>About me</p>
+        <p>Proyects</p>
+        <S.Button>Contact Me 🖖</S.Button>
+      </S.ContainerItems>     
+    </S.ContainerNavbar>
+ ); 
 }
 
 export default Navbar
